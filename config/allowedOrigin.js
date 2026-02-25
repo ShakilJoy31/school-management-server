b9bae8be0326063f49d517259740636e3b966b93
ShakilJoy31 <joy31a@gmail.com>
2026-02-26 02:06:06 +0600

    Done

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -1,14 +1,14 @@
 const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
-  "http://admin.newscell.org",
-  "https://admin.newscell.org",
-  "https://www.admin.newscell.org",
-  "http://www.admin.newscell.org",
-  "https://newscell.org",
-  "http://newscell.org",
-  "https://www.newscell.org",
-  "http://www.newscell.org"
+
+
+  "http://schoolbd24.com",
+  "https://schoolbd24.com",
+
+  "https://www.schoolbd24.com",
+  "http://www.schoolbd24.com",
+
 ];
 
 module.exports = allowedOrigins;
